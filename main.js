1cc081afd93d72d57b443bb84ef3d3b178eb4557
Jérémy Gendre <jeremy@atipik.ch>
2024-03-02 17:22:45 +0100

    feat: add parallax effect

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log('tg')
+document.addEventListener('DOMContentLoaded', function() {
+  const images = document.querySelectorAll('.parallax-image');
+  const hr = document.querySelector('#parallax-name hr');
+
+  document.addEventListener('scroll', function() {
+    const scroll = window.scrollY;
+    const header = document.querySelector('header');
+    const windowHeight = window.innerHeight;
+    if (scroll > windowHeight / 2) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+
+    images.forEach(image => {
+      const speed = image.dataset.speed || 1;
+      image.style.transform = `translateY(${scroll * (speed)}px)`;
+    });
+    console.log(scroll)
+    let percentage = 30 + (scroll / windowHeight) * 20;
+    hr.style.setProperty('width', `${percentage}%`, 'important');
+  });
+
+  document.addEventListener('mousemove', function(e) {
+    let x = Math.abs(e.clientX - window.innerWidth / 2);
+    if(x < window.innerWidth / 6) {
+      x = window.innerWidth / 6;
+    } 
+  });
+});
