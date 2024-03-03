e512d4352ea659db46e001427df210ad3ca74e37
Jérémy Gendre <jeremy@atipik.ch>
2024-03-03 18:58:26 +0100

    feat: add sections

--- main.js
+++ main.js
@@ -1,24 +1,48 @@
+function scrollToSection(section) {
+  const el = document.querySelector(section);
+  const top = el.offsetTop;
+  window.scrollTo({
+    top: top + window.innerHeight,
+    behavior: 'smooth'
+  });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   const images = document.querySelectorAll('.parallax-image');
   const hr = document.querySelector('#parallax-name hr');
+  const sections = document.querySelectorAll('section');
+  const navLinks = document.querySelectorAll('nav ul li');
 
   document.addEventListener('scroll', function() {
     const scroll = window.scrollY;
     const header = document.querySelector('header');
     const windowHeight = window.innerHeight;
-    if (scroll > windowHeight / 2) {
+    if (scroll > windowHeight / 1.3) {
       header.classList.add('scrolled');
     } else {
       header.classList.remove('scrolled');
     }
 
+    // parallax effect
     images.forEach(image => {
       const speed = image.dataset.speed || 1;
       image.style.transform = `translateY(${scroll * (speed)}px)`;
     });
-    console.log(scroll)
+
+    // animate the separator
     let percentage = 30 + (scroll / windowHeight) * 20;
     hr.style.setProperty('width', `${percentage}%`, 'important');
+
+    // animate the section when it's in view
+    sections.forEach(section => {
+      const top = section.offsetTop;
+      const height = section.offsetHeight;
+      if (scroll - window.innerHeight / 2 > top) {
+        section.classList.add('in-view');
+      } else {
+        section.classList.remove('in-view');
+      }
+    });
   });
 
   document.addEventListener('mousemove', function(e) {
@@ -26,5 +50,14 @@ document.addEventListener('DOMContentLoaded', function() {
     if(x < window.innerWidth / 6) {
       x = window.innerWidth / 6;
     } 
+  });
+
+  // smooth scroll on nav link
+  navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      const section = link.dataset.href;
+      scrollToSection(section);
+    });
   });
 });
